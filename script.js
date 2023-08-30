02f80aad1df38073652e20cb786651205df6181b
kevin <kevindubon43@gmail.com>
2023-08-29 17:08:25 -0700

    Started Library project but changed it to movies instead of books. Haven't started on js portion.

---
+++ script.js
@@ -0,0 +1,15 @@
+const addMovieBtn = document.querySelector('#addMovie');
+
+
+const myLibrary = [];
+
+function Movie() {
+
+}
+
+
+function addMovieToLibrary() {
+    
+}
+
+//If movie is watched, make background color green, if movie has not been watched, leave alone?//
