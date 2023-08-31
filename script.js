b993240680be764d340b5168bf20545ca185aa74
kevin <kevindubon43@gmail.com>
2023-08-30 20:34:05 -0700

    Added logic to the add movie button. Button will make a form pop up to fill movie info, if button is pressed again, form will disappear.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const addBtn = document.querySelector('#addMovieBtn');
 const main = document.querySelector('.main');
+const movieBtn = document.querySelector('#addMovieBtn');
 const addedMovie = document.createElement('div');
+const movieForm = document.querySelector('#movieInfo');
 
 const myLibrary = [];
+
+movieForm.style.display = 'none';
 
 function Movie(title, director, runtime, watched) {
     this.title = title
@@ -10,6 +13,9 @@ function Movie(title, director, runtime, watched) {
     this.runtime = runtime
     this.watched = watched
 }
+
+
+
 //displayMovie() should add the title, director, runtime, and watched parameters to a div that has 4 sections.//
 //forEach loop?//
 function displayMovie() {
@@ -30,3 +36,19 @@ function addMovieToLibrary() {
 
 //If movie is watched, make background color green, if movie has not been watched, leave alone?//
 //Create a form that takes all 4 parameters but make it hidden until button is pressed?//
+
+
+
+function showForm() {
+       movieForm.style.display = movieForm.style.display === 'none' ? 'flex' : 'none'; 
+       if(movieForm.style.display === 'none') {
+        movieBtn.textContent = '+'
+       }
+       else if (movieForm.style.display === 'flex') {
+        movieBtn.textContent = 'X'
+    }
+    }
+
+movieBtn.addEventListener('click', () => {
+    showForm();
+});
